7eaf2c51fb15a662d7cb0ffb03601299d838e2a2
hng011 <hansnaufal.3040@gmail.com>
2025-06-18 11:27:47 +0700

    add apitf sh to push docker image

--- api-tf2180/pushimage2reg.sh
+++ api-tf2180/pushimage2reg.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
 source .env
+
+VERSION=""
+
+while getopts "t:" opt; do
+  case $opt in
+    t)
+      VERSION="$OPTARG"
+      echo "Version set to: $VERSION"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+az acr login --name ${AZURE_REGISTERY_NAME}
+
+echo "Pushing ${LOCAL_REPO}/${IMAGE_NAME}:$VERSION to ${ACR_REPO}/${IMAGE_NAME}:$VERSION"
+docker push ${ACR_REPO}/${IMAGE_NAME}:$VERSION
+
+echo "Pushing ${LOCAL_REPO}/${IMAGE_NAME}:latest to ${ACR_REPO}/${IMAGE_NAME}:latest"
+docker push ${ACR_REPO}/${IMAGE_NAME}:latest
